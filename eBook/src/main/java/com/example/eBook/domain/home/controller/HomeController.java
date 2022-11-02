9868bf411f5082f77e94e768e7386a3c393a14b0
minseong <kdsvip5@naver.com>
2022-11-02 17:45:17 +0900

    Feat : 관리자 홈 컨트롤러 추가(#33)
    - 일반 홈 컨트롤러 매핑 위치 변경

---
+++ eBook/src/main/java/com/example/eBook/domain/home/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.example.eBook.domain.home.controller;
+
+import com.example.eBook.domain.post.dto.PostDto;
+import com.example.eBook.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final PostService postService;
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/")
+    public String showCommonHome(Model model) {
+        List<PostDto> postList = postService.findRecentTop100();
+
+        model.addAttribute("postList", postList);
+        return "home/home";
+    }
+
+    @PreAuthorize("isAuthenticated() and hasAnyAuthority('ADMIN')")
+    @GetMapping("/adm/home/main")
+    public String showAdminHome(Model model) {
+        // 구현 필요
+
+        return "home/adminHome";
+    }
+}
