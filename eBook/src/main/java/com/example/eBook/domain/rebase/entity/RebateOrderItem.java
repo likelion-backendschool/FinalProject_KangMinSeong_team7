293c96c958525bed3c1388d6ffa0a868db41556f
minseong <kdsvip5@naver.com>
2022-11-02 17:40:41 +0900

    Feat : 정산처리를 위한 기본 코드 작성(#33)
    - RebateOrderItem Entity, Controller, Service, Repository 추가
    - Controller 로직 구현
    - Service, Repository 로직 구현전
    - View 구현전

---
+++ eBook/src/main/java/com/example/eBook/domain/rebase/entity/RebateOrderItem.java
@@ -0,0 +1,109 @@
+package com.example.eBook.domain.rebase.entity;
+
+import com.example.eBook.domain.base.BaseTimeEntity;
+import com.example.eBook.domain.cash.entity.CashLog;
+import com.example.eBook.domain.member.entity.Member;
+import com.example.eBook.domain.order.entity.Order;
+import com.example.eBook.domain.order.entity.OrderItem;
+import com.example.eBook.domain.product.entity.Product;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class RebateOrderItem extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private OrderItem orderItem;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Product product;
+
+    // 가격
+    private int price;
+    private int salePrice;
+    private int wholesalePrice;
+    private int pgFee;
+    private int payPrice;
+    private int refundPrice;
+    private boolean isPaid;
+    private LocalDateTime payDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private CashLog rebateCashLog;
+    private LocalDateTime rebateDate;
+
+    private String productSubject;
+
+    private LocalDateTime orderItemCreateDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member buyer;
+    private String buyerName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member seller;
+    private String sellerName;
+
+    public RebateOrderItem(OrderItem orderItem) {
+        this.orderItem = orderItem;
+        order = orderItem.getOrder();
+        product = orderItem.getProduct();
+        price = orderItem.getPrice();
+        salePrice = orderItem.getSalePrice();
+        wholesalePrice = orderItem.getWholesalePrice();
+        pgFee = orderItem.getPgFee();
+        payPrice = orderItem.getPayPrice();
+        refundPrice = orderItem.getRefundPrice();
+        isPaid = orderItem.isPaid();
+        payDate = orderItem.getPayDate();
+
+        productSubject = orderItem.getProduct().getSubject();
+
+        orderItemCreateDate = orderItem.getCreateDate();
+
+        buyer = orderItem.getOrder().getMember();
+        buyerName = orderItem.getOrder().getMember().getUsername();
+
+        seller = orderItem.getProduct().getMember();
+        sellerName = orderItem.getProduct().getMember().getNickname();
+    }
+}
