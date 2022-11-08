12ee7e4137b19934ee7adb90e22b0e42690e887a
minseong <kdsvip5@naver.com>
2022-11-09 04:34:51 +0900

    Feat : ADMIN 로그인시 관리자 홈 페이지로(#35)
    - 관리자 권한을 가진 유저가 로그인시 관리자 홈 페이지로
    - 관리자 layout 수정

--- eBook/src/main/java/com/example/eBook/global/config/security/AuthSuccessHandler.java
+++ eBook/src/main/java/com/example/eBook/global/config/security/AuthSuccessHandler.java
@@ -2,7 +2,9 @@ package com.example.eBook.global.config.security;
 
 import com.example.eBook.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +12,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final MemberRepository memberRepository;
@@ -20,6 +25,18 @@ public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         memberRepository.updateLastLoginTime(authentication.getName());
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+
+        while (iter.hasNext()) {
+            GrantedAuthority auth = iter.next();
+            if (auth.getAuthority().equals("ROLE_ADMIN")) {
+                setDefaultTargetUrl("/adm/home/main");
+                super.onAuthenticationSuccess(request, response, authentication);
+            }
+        }
+
         setDefaultTargetUrl("/");
         super.onAuthenticationSuccess(request, response, authentication);
     }
